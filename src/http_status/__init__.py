5a3f423a0843d617b8b31f1ad9c85bb359167856
nueverest <nu.everest@gmail.com>
2014-06-01 13:50:38 -0700

    Updated status code and source information
    Source 1: Hypertext Transfer Protocol -- HTTP/1.1 RFC 2616 Fielding, et al.
              http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
    
    Source 2: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
    
    Source of the status code and description is assumed to be Source 1 unless otherwise noted.

--- src/http_status/__init__.py
+++ src/http_status/__init__.py
@@ -60,33 +60,40 @@ This can be changed by passing arguments ``name_fail`` and
 The class ``NoneStatus`` is exactly the same as Status, but ``name_fail``
 and ``description_fail`` both default to None."""
 
+# Source 1: Hypertext Transfer Protocol -- HTTP/1.1 RFC 2616 Fielding, et al.
+#           http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+# Source 2: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+# Source of the status code and description is assumed to be Source 1 unless otherwise noted.
+
 # Names of HTTP status codes.
 name = {
-
     # Informational.
     100: 'Continue',
     101: 'Switching Protocols',
     102: 'Processing',
-    103: 'Checkpoint',
-    122: 'Uri Too Long',
+
+    # Success
     200: 'OK',
     201: 'Created',
     202: 'Accepted',
-    203: 'Non-Authoritative Info',
+    203: 'Non-Authoritative Information',
     204: 'No Content',
     205: 'Reset Content',
     206: 'Partial Content',
-    207: 'Multi-Status',
-    208: "I'm Used",
+    207: 'Multi-Status',        # WebDAV; RFC 4918
+    208: "Already Reported",    # WebDAV; RFC 5842
+    226: "IM Used",             # RFC 3229
+
+    # Redirection
     300: 'Multiple Choices',
     301: 'Moved Permanently',
     302: 'Found',
     303: 'See Other',
     304: 'Not Modified',
     305: 'Use Proxy',
-    306: 'Switch Proxy',
+    306: 'Switch Proxy',        # HTTP/1.1 No longer used
     307: 'Temporary Redirect',
-    308: 'Resume Incomplete',
+    308: 'Permanent Redirect',  # Approved as experimental RFC http://en.wikipedia.org/wiki/Request_for_Comments
 
     # Client Error.
     400: 'Bad Request',
@@ -107,19 +114,26 @@ name = {
     415: 'Unsupported Media Type',
     416: 'Requested Range Not Satisfiable',
     417: 'Expectation Failed',
-    418: "I'm a Teapot",
-    422: 'Unprocessable Entity',
-    423: 'Locked',
-    424: 'Failed Dependency',
-    425: 'Unordered Collection',
-    426: 'Upgrade Required',
-    428: 'Precondition Required',
-    429: 'Too Many Requests',
-    431: 'Header Fields Too Large',
-    444: 'No Response',
-    449: 'Retry With',
-    450: 'Blocked By Windows Parental Controls',
-    499: 'Client Closed Request',
+    418: 'I\'m a Teapot',               # RFC 2324 April Fool's Joke
+    419: 'Authentication Timeout',      # RFC 2616 not part of HTTP standard
+    422: 'Unprocessable Entity',        # WebDAV; RFC 4918
+    423: 'Locked',                      # WebDAV; RFC 4918
+    424: 'Failed Dependency',           # WebDAV; RFC 4918
+    425: 'Unordered Collection',        # Defined in drafts of "WebDAV Advanced Collections Protocol"
+    426: 'Upgrade Required',            # RFC 2817
+    428: 'Precondition Required',       # RFC 6585 http://tools.ietf.org/html/rfc6585
+    429: 'Too Many Requests',           # RFC 6585 http://tools.ietf.org/html/rfc6585
+    431: 'Header Fields Too Large',     # RFC 6585 http://tools.ietf.org/html/rfc6585
+    440: 'Login Timeout',               # Microsoft
+    444: 'No Response',                 # Nginx
+    449: 'Retry With',                  # Microsoft
+    450: 'Blocked By Windows Parental Controls',    # Microsoft
+    451: 'Unavailable For Legal Reasons',           # Internet Draft http://en.wikipedia.org/wiki/HTTP_451
+    494: 'Request Header Too Large',    # Nginx
+    495: 'Cert Error',                  # Nginx
+    496: 'No Cert',                     # Nginx
+    497: 'HTTP to HTTPS',               # Nginx
+    499: 'Client Closed Request',       # Nginx
 
     # Server Error.
     500: 'Internal Server Error',
@@ -128,21 +142,22 @@ name = {
     503: 'Service Unavailable',
     504: 'Gateway Timeout',
     505: 'HTTP Version Not Supported',
-    506: 'Variant Also Negotiates',
-    507: 'Insufficient Storage',
-    509: 'Bandwidth Limit Exceeded',
-    510: 'Not Extended'
+    506: 'Variant Also Negotiates',     # RFC 2295
+    507: 'Insufficient Storage',        # WebDAV; RFC 4918
+    508: 'Loop Detected',               # WebDAV; RFC 5842
+    509: 'Bandwidth Limit Exceeded',    # Apache bw/limited extension
+    510: 'Not Extended',                # RFC 2774
+    511: 'Network Authentication'       # RFC 6585
 }
 
 # Descriptions of HTTP status codes.
 description = {
-
     # Informational.
     100: 'Continue with the request.',
     101: 'Server is switching to a different protocol.',
     102: 'Server has received and is processing the request, but no response is available yet.',
-    #103: ('checkpoint',),
-    #122: ('uri_too_long', 'request_uri_too_long'),
+
+    # Success
     200: 'Request was successful.',
     201: 'Request was successful, and a new resource has been created.',
     202: 'Request has been accepted but not yet acted upon.',
@@ -151,7 +166,10 @@ description = {
     205: 'Server successfully processed the request, but is not returning any content.',
     206: 'Download is separated into multiple streams, due to range header.',
     207: 'Message body that follows is an XML message and can contain a number of separate response codes.',
-    208: 'Response is a representation of the result of one or more instance-manipulations applied to the current instance.',
+    208: ('Response is a representation of the result of one or more instance-manipulations applied to the current ' +
+          'instance.'),
+    226: ('The server has fulfilled a GET request for the resource, and the response is a representation of the ' +
+          'result of one or more instance-manipulations applied to the current instance.'),
 
     # Redirection.
     300: 'Request has more than one possible response.',
@@ -162,7 +180,7 @@ description = {
     305: 'Requested resource may only be accessed through a given proxy.',
     306: 'No longer used. Requested resource may only be accessed through a given proxy.',
     307: 'URI of this resource has changed, temporarily. Use the same HTTP method to access it.',
-    #308: ('resume_incomplete', 'resume'),
+    308: 'The request, and all future requests should be repeated using another URI.',
 
     # Client Error.
     400: 'Server could not understand the request, due to invalid syntax.',
@@ -192,9 +210,16 @@ description = {
     428: 'Origin server requires the request to be conditional.',
     429: 'User has sent too many requests in a given amount of time.',
     431: 'Server rejected the request because either a header, or all the headers collectively, are too large.',
+    440: 'Your session has expired. (Microsoft)',
     444: 'Server has returned no information to the client and closed the connection (Ngnix).',
     449: 'Request should be retried after performing the appropriate action (Microsoft).',
     450: 'Windows Parental Controls are turned on and are blocking access to the given webpage.',
+    451: ('You attempted to access a Legally-restricted Resource. This could be due to censorship or ' +
+          'government-mandated blocked access.'),
+    494: 'Nginx internal code',
+    495: 'SSL client certificate error occurred. (Nginx)',
+    496: 'Client did not provide certificate (Nginx)',
+    497: 'Plain HTTP request sent to HTTPS port. (Nginx)',
     499: 'Connection has been closed by client while the server is still processing its request (Nginx).',
 
     # Server Error.
@@ -206,9 +231,12 @@ description = {
     505: 'HTTP version used in the request is not supported by the server.',
     506: 'Transparent content negotiation for the request results in a circular reference.',
     507: 'Server is unable to store the representation needed to complete the request.',
+    508: 'The server detected an infinite loop while processing the request',
     #509: 'This status code, while used by many servers, is not specified in any RFCs.',
     510: 'Further extensions to the request are required for the server to fulfill it.',
+    511: 'The client needs to authenticate to gain network access.'
 }
+
 
 
 class Status(object):
