cffafbcce4de4eb00d1deb0dc6f947bf5ac8653a
nueverest <nu.everest@gmail.com>
2014-06-01 19:28:15 -0700

    Added __unicode__(verbose)

--- src/http_status/__init__.py
+++ src/http_status/__init__.py
@@ -254,6 +254,16 @@ class Status(Schema):
         self.description_fail = description_fail
         super(Status, self).__init__()
 
+    def __unicode__(self, verbose=False):
+        """
+        __unicode__(verbose=True) returns:  HTTP <code> <name> <description>
+        __unicode__(verbose=False) returns: HTTP <code> <name>
+        """
+        if verbose:
+            return u'HTTP {} {} {}'.format(self.code, self.name, self.description)
+        else:
+            return u'HTTP {} {}'.format(self.code, self.name)
+
     @property
     def name(self):
         """Return the name of the current status code as a string."""
